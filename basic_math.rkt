a4ebd8dedc27004263c7472cdb03b1f6909bd2dc
cnkra <canhuseyinkara@gmail.com>
2025-02-21 05:47:10 +0100

    Added basic_math.rkt for arithmetic operations

---
+++ basic_math.rkt
@@ -0,0 +1,30 @@
+#lang racket
+(+ 25 50)
+
+(* (+ 10 5) (+ 10 5))
+
+(+ 1 (+ 1 (+ 1 1) 2) 3 4 5)
+
+(+ 1 5 3 4 5)
+
+(sin 0)
+
+(define x 3)
+(define y 4)
+
+x
+y
+
+(define z (+ (* x x) (* y y)))
+
+(sqrt z)
+(expt z (/ 1 2))
+
+(expt z (/ 1 3))
+
+"darquesse"
+
+(define d "darquesse")
+d
+
+(string-append d " study now")
